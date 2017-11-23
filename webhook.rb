a1767fbcef49a5f057c2954f160bad100de9d232
jsugarman <joel.sugarman@digital.justice.gov.uk>
2017-11-23 00:53:20 +0000

    [WIP] Add command definitions, bsic response and error handling

--- webhook.rb
+++ webhook.rb
@@ -10,14 +10,66 @@ class Webhook < Sinatra::Base
 
   post '/webhook' do
     content_type 'application/json'
-    body webhook_response
+    begin
+      # TODO: implement slack token
+      # raise SecurityError, 'Please check the value of `WEBHOOK_TOKEN` in the environment' unless (ENV['WEBHOOK_TOKEN'] || !params[:token].nil? && params[:token].eql?(ENV['WEBHOOK_TOKEN']))
+
+      command = Command.new(params[:command])
+      body command.response
+    rescue SecurityError => err
+      body error_reponse(err)
+    rescue CommandError => err
+      body error_reponse(err) + error_reponse(Command.usages)
+    end
   end
 
   private
 
-  def webhook_response
+  def error_reponse err = nil
     {
-      message: ':robot: ping-bot message test'
+      message: err
+    }.to_json
+  end
+end
+
+class CommandError < StandardError; end
+
+class Command
+  USAGES = [
+      '@pingbot ping &lt;domain-name.co.uk&gt;',
+      '@pingbot ping &lt;domain-name.co.uk&gt;[,&lt;other-domain.dsd.io&gt;]'
+  ].map { |usage| "`#{usage}`" }.freeze
+
+  attr_reader :command
+
+  def initialize command
+    @command = command
+  end
+
+  def response
+    case
+    when command.match?(/@ping-?bot\s+ping\s+([\w\d\.-])+(,\s?[\w\d\.-]+)*/i)
+      ping
+    when command.match?(/@ping-?bot\s+help/i)
+      help
+    else
+      raise CommandError, "#{command} is invalid" unless command.match? /@ping-?bot\s+ping\s+([\w\d\.-])+(,\s?[\w\d\.-]+)*/i
+    end
+  end
+
+  private
+
+  def self.usages
+    { message: "Say one of the following: #{USAGES.join(', ')}" }.to_json
+  end
+
+  def help
+    self.class.usages
+  end
+
+  def ping
+    {
+      message: ':robot: ping-bot pinging... test'
     }.to_json
   end
 end
